c0c1bd33d00b3876b5a7175f16a46833281d0e74
jawhar <jawhardkebbi@gmail.com>
2023-12-09 03:52:30 +0100

    removed the secret key from the settings file

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from .secret_key import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m@xbg^^2909z3bd03(ab8t+$agw+_a%att5$_*l%u(#%5-tz91"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
